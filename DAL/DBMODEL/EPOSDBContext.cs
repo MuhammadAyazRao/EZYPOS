1ba4dad8ac50513d749b8c378daf0e7b6fa46dd7
Sikandar Abbas <ayaz.mehmood4070@gamil.com>
2022-05-08 23:35:58 +0500

    Ip address in datastring and document

--- DAL/DBMODEL/EPOSDBContext.cs
+++ DAL/DBMODEL/EPOSDBContext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-
+using System.Net;
 #nullable disable
 
 namespace DAL.DBMODEL
@@ -56,8 +56,11 @@ namespace DAL.DBMODEL
         {
             if (!optionsBuilder.IsConfigured)
             {
+                // Get the IP
+                string myIP = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+                
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-E7Q1BER\\SQLEXPRESS;Database=EPOS-DB;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Data Source="+myIP+";Database=EPOS-DB;Trusted_Connection=True;");
             }
         }
 
